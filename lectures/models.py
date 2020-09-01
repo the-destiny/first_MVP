b69a5d94e7ff55d2e876c867be4de19a9d17a9b2
JeeHyungPark <jhp0227@naver.com>
2020-09-02 01:36:49 +0900

    [#8] Add slug to LectureModel

--- lectures/models.py
+++ lectures/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Lecture(models.Model):
 
@@ -27,8 +28,7 @@ class Lecture(models.Model):
         InDesign = 'InDesign', 'InDesign'
         Illustrator = 'Illustrator', 'Illustrator'
         Sketch = 'Sketch', 'Sketch'
-
-
+        
     title = models.CharField(
         max_length=100,
         unique=True,
@@ -66,6 +66,12 @@ class Lecture(models.Model):
     updated_at = models.DateTimeField(
         auto_now=True,
     )
+
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True, blank=True)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.sub_category)
+        super(Lecture, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = '강의'
