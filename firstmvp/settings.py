878d8904d3c7a48611ebe7b552e8f0c00ec6f51e
Hyo Gyeong <cdkrcd8@gmail.com>
2020-09-03 00:28:44 +0900

    HomeComplete

--- firstmvp/settings.py
+++ firstmvp/settings.py
@@ -85,8 +85,18 @@ USE_TZ = True
 AUTH_USER_MODEL = 'accounts.User'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'home', 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'home', 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+SASS_PROCESSOR_ENABLED = True
+
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
