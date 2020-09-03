8a5ba2e66d1497995510117b684f1c15fff95d2f
Hyo Gyeong <cdkrcd8@gmail.com>
2020-09-03 22:39:54 +0900

    [#7] rebase 25/27

--- lectures/views.py
+++ lectures/views.py
@@ -6,4 +6,8 @@ def detail(request, lecture_slug):
     lecture = get_object_or_404(Lecture, slug=lecture_slug)
     url = lecture.video
     lectureId = url.split('=')[1]
+<<<<<<< HEAD
+=======
+
+>>>>>>> 10b88e5... [#6] Implement detailpage
     return render(request, 'detail.html', {'lecture':lecture, 'lectureId':lectureId})
