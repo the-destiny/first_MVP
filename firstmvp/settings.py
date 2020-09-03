ddb7d77afe8f96fc1f1c5099c81f13b02cade722
Hyo Gyeong <cdkrcd8@gmail.com>
2020-09-03 18:57:36 +0900

    correct px, bg-img, indentation etc by reviews

--- firstmvp/settings.py
+++ firstmvp/settings.py
@@ -88,6 +88,7 @@ AUTH_USER_MODEL = 'accounts.User'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'home', 'static'),
+    os.path.join(BASE_DIR, 'lecture', 'static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- home/views.py
+++ home/views.py
@@ -2,4 +2,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'homepage.html')
-
